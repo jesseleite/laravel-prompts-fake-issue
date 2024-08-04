94a8faa372f059feadca13be68d05f13330f2621
Jesse Leite <jesseleite@gmail.com>
2024-08-04 15:59:44 -0400

    Workaround using late static binding.

--- tests/CommandTest.php
+++ tests/CommandTest.php
@@ -3,13 +3,20 @@
 namespace Tests;
 
 use Laravel\Prompts\Key;
-use Laravel\Prompts\Prompt;
+use Laravel\Prompts\Prompt as LaravelPrompt;
 use PHPUnit\Framework\Attributes\Test;
 
 use function Laravel\Prompts\confirm;
 
 class CommandTest extends TestCase
 {
+    public function tearDown(): void
+    {
+        Prompt::resetFallbacks();
+
+        parent::tearDown();
+    }
+
     #[Test]
     public function it_tests_laravel_artisan_command()
     {
@@ -36,5 +43,18 @@ class CommandTest extends TestCase
         $result = confirm(label: 'Are you sure?');
 
         $this->assertTrue($result);
+    }
+}
+
+class Prompt extends LaravelPrompt
+{
+    public static function resetFallbacks()
+    {
+        static::$shouldFallback = false; // Late static binding hack
+    }
+
+    public function value(): mixed
+    {
+        // Abstract class requires us to define this method
     }
 }
